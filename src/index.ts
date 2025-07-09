8396f08a3c19a8fa163df7f7d40df1ead9cd7eba
odahiroomi <odahiroomi@gmail.com>
2025-07-09 11:12:24 +0900

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -126,6 +126,7 @@ app.listen(PORT, () => {
 //   res.redirect('/');
 // });
 
+// // サーバーを起動する
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
